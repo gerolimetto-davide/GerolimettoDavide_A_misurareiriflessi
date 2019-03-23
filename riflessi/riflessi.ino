9b9b3410e795eb0520331a2026eac1ef06db7958
gerolimetto-davide <davie.gerolimetto@barsanti.edu.it>
2019-03-23 16:28:28 +0100

    progetto finito 23/03/2019

--- riflessi/riflessi.ino
+++ riflessi/riflessi.ino
@@ -1,91 +1,113 @@
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(12,11,5,4,3,2);
-int ledR = 7;
-int bottone = 8;
-int buzzer = 9;
-int scelta = 0;
+LiquidCrystal lcd(2,3,4,5,6,7);
+const int ledR = 12;
+const int bottone = 13;
+const int buzzer = 11;
+
+long tempoLed = 0;
+long tempoBuzzer = 0;
 
 void setup() {
   // put your setup code here, to run once:
-pinMode(bottone, INPUT);
-millis();
-pinMode(buzzer,OUTPUT);
-analogWrite(ledR, OUTPUT);
-lcd.begin(16,2);
-lcd.print("arduino Timer");}
+  pinMode(bottone, INPUT);
+  //millis();
+  pinMode(buzzer,OUTPUT);
+  analogWrite(ledR, OUTPUT);
+  lcd.begin(16,2);
+  lcd.print("Arduino Timer");
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
-start();
-riflessiConLed();
-riflessiConBuzzer();
-
-
+  while(digitalRead(bottone) == LOW);
+  start();
+  riflessiConLed();
+  riflessiConBuzzer();
+  stampaMedia();
+  riavvioRiflessi();
 }
 
 void start()
 {
   lcd.setCursor(0,0);
-  lcd.print("riflessi");
+  lcd.print("Riflessi"); 
   lcd.setCursor(0,1);
   lcd.print("premi il bottone");
-  lcd.setCursor(0,1);
-  lcd.print("premi il bottone per iniziare");
-
-  while (digitalRead(bottone) == LOW)
-  {
-    
-  }
+  while (digitalRead(bottone) == LOW) {};
   lcd.setCursor(0,0);
-  lcd.print("avvio del test");
+  lcd.print("Avvio del test");
 
 }
 void riflessiConLed()
 {
-  lcd.setCursor(0,0);
-  lcd.print("Primo avvio");
   lcd.setCursor(0,1);
   lcd.print("test con led");
   delay(2000);
-  
   lcd.setCursor(0,0);
   lcd.print("Schiaccia il bottone");
-  lcd.setCursor(0,0);
-  lcd.print("accensione del led");
-  lcd.setCursor(0,1);
   lcd.print("led rosso");
   delay(1500);
   lcd.clear();
-  int tempo = random(500,10000);
+  long tempo = random(500,1000);
+  long tempoMillis = millis() + tempo;
   delay (tempo);
   digitalWrite(ledR, HIGH);
-  delay(2000);
+  delay(200);
   digitalWrite(ledR, LOW); 
-  lcd.print("Tempo test");
+  while (digitalRead(bottone) == LOW) {};
+  tempoLed = millis() - tempoMillis;
+  
+  lcd.clear();
+  lcd.print("Tempo test: ");
+  lcd.print(tempoLed);
   delay(3000);
 }
+
 void riflessiConBuzzer()
 {
-  lcd.setCursor(0,0);
-  lcd.print("secondo avvio");
   lcd.setCursor(0,1);
-  lcd.print("accensione del buzzer");
-  delay(3000);
+  lcd.print("test con led");
+  delay(2000);
   lcd.setCursor(0,0);
   lcd.print("Schiaccia il bottone");
-  lcd.setCursor(0,1);
-  lcd.print("bottone all'accensione");
-  delay(1000);
-  lcd.setCursor(0,0);
-  lcd.print("accensione del buzzer");
-  lcd.setCursor(0,1);
-  lcd.print("suono");
-  delay(2000);
-  int tempo = random(500,5000);
+  lcd.print("led rosso");
+  delay(1500);
+  lcd.clear();
+  long tempo = random(500,1000);
+  long tempoMillis = millis() + tempo;
   delay (tempo);
-  tone(buzzer,500,1500);
-  lcd.print("Tempo test");  
+  tone(buzzer,1000,300); 
+  while (digitalRead(bottone) == LOW);
+  tempoBuzzer = millis() - tempoMillis;
+  lcd.clear();
+  lcd.print("Tempo test: ");
+  lcd.print(tempoBuzzer);
   delay(3000);
-  
+}
 
+void stampaMedia()
+{
+
+  lcd.setCursor(0,0);
+  lcd.print("Media :");
+  long media = tempoBuzzer + tempoLed;
+  media = media / 2.0;
+  lcd.print(media);
+  delay(3000);
+  if(media > 250)
+  {
+  lcd.setCursor(0,1);
+  lcd.print("Non hai passato il test");
+  delay(3000);
+  }
+  if(media < 250)
+  {
+  lcd.setCursor(0,1);
+  lcd.print(" Hai passato il test");
+  delay(3000);
+  }
+}
+void riavvioRiflessi()
+{
+    setup();
 }
